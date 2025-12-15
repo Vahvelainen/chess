685d9361224126b4e7d2cc772c7e2f8e403f4927
Vahvelainen <leevi.vahvelainen@gmail.com>
2025-12-15 21:58:51 +0200

    Add GitHub link to footer

--- src/views/Footer.tsx
+++ src/views/Footer.tsx
@@ -3,6 +3,13 @@ import React from "react";
 export function Footer(): React.JSX.Element {
   return (
     <footer className="site-footer">
+      <a
+        href="https://github.com/Vahvelainen/chess"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GitHub
+      </a>{" | "}
       Chess piece icons by{" "}
       <a href="https://fontawesome.com" target="_blank" rel="noopener noreferrer">
         Font Awesome
